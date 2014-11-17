b56fdcfb7a41e4071fa9137ef800c1704bad894c
jfxvii <jeremyf@sfu.ca>
2014-11-17 13:54:42 -0800

    changes from mason
    commit changed

--- js/application.js
+++ js/application.js
@@ -1,11 +1,7 @@
+var lead;
 /*
-	Stop the melody being played
+    Stop the melody being played
  */
-
-    var notesLead = generateMelody();
-           var test = synthastico.createSynth(
-                audioContext, notesLead
-            );
 $(".stop-button").click(function() {
     test.disconnect();
 });
@@ -14,24 +10,9 @@ $(".stop-button").click(function() {
     Play the new melody
  */
 $(".play-button").click(function() {
-
-           test.sound = function (note, time) {
-                var val =
-                    440 * Math.pow(2, (note.tone - 36) / 12) * (time / 48000);
-
-                var amp = synthastico.ampFromADSR(
-                    note.totalPlayed,
-                    50*(48000 / 1000),
-                    50*(48000 / 1000),
-                    1,
-                    1000*(48000 / 1000)
-                )
-                return Math.sin(val)*amp;
-            }
-    test.connect(delay);
-        delay.connect(audioContext.destination);
+    var notesLead = generateMelody();
+      // AudioContext and List of nodes
+      lead = synthastico.createSynth(audioContext, notesLead);
+      lead.sound = leadSound();
+      lead.connect(audioContext.destination);
 });
-
-/*
-
-
