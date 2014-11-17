4f840671bf68d3604d75a77279bb15fc824242f4
jfxvii <jeremyf@sfu.ca>
2014-11-17 00:24:28 -0800

    application changes
    start playback with play button,
    stop playback with the stop button

--- js/application.js
+++ js/application.js
@@ -2,18 +2,36 @@
 	Stop the melody being played
  */
 
+    var notesLead = generateMelody();
+           var test = synthastico.createSynth(
+                audioContext, notesLead
+            );
 $(".stop-button").click(function() {
-	lead.disconnect();
+    test.disconnect();
 });
 
 /*
-	Play the new melody
+    Play the new melody
  */
-// $(".play-button").click(function() {
-// 	var notesLead = generateMelody();
-// 	notesLead.connect(delay);
-// });
+$(".play-button").click(function() {
+
+           test.sound = function (note, time) {
+                var val =
+                    440 * Math.pow(2, (note.tone - 36) / 12) * (time / 48000);
+
+                var amp = synthastico.ampFromADSR(
+                    note.totalPlayed,
+                    50*(48000 / 1000),
+                    50*(48000 / 1000),
+                    1,
+                    1000*(48000 / 1000)
+                )
+                return Math.sin(val)*amp;
+            }
+    test.connect(delay);
+        delay.connect(audioContext.destination);
+});
 
 /*
-	
- 
+
+
